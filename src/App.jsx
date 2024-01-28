634eec460fc5a663f78ca16596212cf9d7ed1d20
fathonaji <fathona21@gmail.com>
2024-01-28 15:18:20 +0700

    create form and handle submit mechanism to check how to handle form input in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 2, description: "Charger", quantity: 1, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
 ];
 function App() {
   return (
@@ -19,10 +19,22 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option key={num} value={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Items..." />
+      <button>Add</button>
+    </form>
   );
 }
 
