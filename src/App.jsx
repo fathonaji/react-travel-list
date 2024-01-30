1223237173d57ea95d8d5c7bea89fbae85528302
fathonaji <fathona21@gmail.com>
2024-01-30 22:41:11 +0700

    create conditional rendering on footer if items is empty, if item packed is 100%, and if item packed or listed

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -105,10 +105,23 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+  const totalItems = items.length;
+  const totalPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((totalPacked / totalItems) * 100);
   return (
     <footer className="stats">
-      <em>🧳 You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go ✈️"
+          : `🧳 You have ${totalItems} items on your list, and you already packed ${totalPacked}(${percentage}%)`}
+      </em>
     </footer>
   );
 }
